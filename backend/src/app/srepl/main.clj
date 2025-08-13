c1221414bcdbed40c1b74bb886b45f27533f769c
Andrey Antukh <niwi@niwi.nz>
2025-08-13 12:41:43 +0200

    WIP

--- backend/src/app/srepl/main.clj
+++ backend/src/app/srepl/main.clj
@@ -617,7 +617,7 @@
           (db/exec! conn ["SET statement_timeout = 0"])
           (db/exec! conn ["SET idle_in_transaction_session_timeout = 0"])
 
-          (->> (db/plan conn [query] {:fetch-size 1})
+          (->> (db/plan conn [query] {:fetch-size max-jobs})
                (transduce (take max-items)
                           (completing process-item*)
                           0)))]
